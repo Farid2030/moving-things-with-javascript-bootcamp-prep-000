3dc9e0b85d7f4e92ee42f7026b77a487594147d5
Farid Mansour Thabet Alabsi <alabsifarid@gmail.com>
2018-06-25 20:10:41 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -1,18 +1,42 @@
-const expect = require('expect')
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Rock Dodger! (Beta)</title>
+    <link rel="stylesheet" type="text/css" href="index.css">
+  </head>
 
-const fs = require('fs')
-const jsdom = require('mocha-jsdom')
-const path = require('path')
-
-
-describe('index', () => {
-  
-  jsdom({
-    src: fs.readFileSync(path.resolve(__dirname, '..', 'index.js'), 'utf-8')
-  })
-  
-
-  it('runs', () => {
-    expect(true).to.be.true
-  })
-})
+  <body>
+    <div id="game">
+      <div id="dodger" style="bottom: 0px; left: 180px;"></div>
+    </div>
+    <!-- <script src="index.js"></script> -->
+    <script>
+    var dodger = document.getElementById('dodger')
+    dodger.style.backgroundColor = '#FF69B4'
+    document.addEventListener('keydown', function(e) {
+      switch (e.which) {
+        case 37:
+          moveDodgerLeft()
+          break
+        case 39:
+          moveDodgerRight()
+          break
+      }
+    })
+    function moveDodgerLeft() {
+      var leftNumbers = dodger.style.left.replace('px', '')
+      var left = parseInt(leftNumbers, 10)
+      if (left > 0) {
+        dodger.style.left = `${left - 5}px`
+      }
+    }
+    function moveDodgerRight() {
+      var rightNumbers = dodger.style.right.replace('px', '')
+      var right = parseInt(rightNumbers, 10)
+      if (right > 0) {
+        dodger.style.right = `${right - 5}px`
+      }
+    }
+    </script>
+  </body>
+</html>
